4377c21cbf13b26449f1a29f65457041d13aef75
Vikas Yadav <thevikas@warren.dreamhost.com>
2013-04-06 22:42:41 -0700

    live commit

--- protected/migrations/m121231_151248_create_person.php
+++ protected/migrations/m121231_151248_create_person.php
@@ -5,7 +5,8 @@ class m121231_151248_create_person extends CDbMigration
 	public function up()
 	{
 		$this->execute("CREATE TABLE  `person` (
-				`id_person` int(11) NOT NULL AUTO_INCREMENT ,					  `created` datetime NOT NULL,
+				`id_person` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	 		        `created` datetime NOT NULL,
 				`updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 				`first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
 				`last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -15,7 +16,7 @@ class m121231_151248_create_person extends CDbMigration
 				`source` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
 				PRIMARY KEY (`id_person`),
 				UNIQUE KEY `email` (`email`)
-		) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
 	}
 
 	public function down()
